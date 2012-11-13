1f321d3eb952a7b50587e5736c2ae72beb8d21f0
Maggie Dawson <mjdawson@oakland.edu>
2012-11-12 20:16:52 -0500

    immoveableobject.php
    Maggies Robot!

---
+++ immoveableojbect.php
@@ -0,0 +1,25 @@
+<?php include ('header.php'); ?>
+
+<h1>Immovable Object</h1>
+<img src="http://www.secs.oakland.edu/~mjdawson/images/filler.jpg" height="300" width="250" border="0" alt="Immoveable Object" align="left">
+We are pleased to announce the Immoveable Object, our most durable robot to date. It is fully armored in plated vibranium for your ultimate protection. One could call this robot almost indestructable.  Frictionless joints allow fluid movement, which is much faster than our previous models. Almost like a real boy [or girl if that tickles your fancy]. In the rare event of critical damage, the Immoveable Object, has a secondary power source which allows it enough time to complete its hard-wired goal, to protect its human at all costs. 
+<p>
+The Immoveable Object, while not equipt with advanced lasers or high powered rifles, makes the perfect added security feature to any family. The Immoveable Object comes pre-loaded with our "Gaurd Dog" software. He great for patroling the yard while you're not home, or spending time with you in the car when you just need someone to be in the passanger seat so you can be in the carpool lane. Send the Immoveable Object off to college with your child, to keep your mind at ease when they are walking back to their dorm from their late night class. What ever the reason why you want the Immoveable Object, its main objective is to keep your safe from harm.
+<p>
+
+<h2>Product Specifications</h2>
+<b>Dimensions:</b> 66 inches tall x 18 inches wide (1.67 m x .45m)    			<br>
+<b><span class="droptext">Robot Mass:<span style="width:500px;">How much the robot weighs.</span></span></b> 180 lbs (81.7 kg)<br>
+<b>Operating temperature range:</b>	50 F to 75 F (10 C to 23.9 C)		<br>
+<b><span class="droptext">Frame:<span style="width:500px;">What the robot is made from.</span></span></b> Vibranium <br>
+<b><span class="droptext">Class:<span style="width:500px;">The general type of robot.</span></span></b> Gaurd Bot <br>
+<b><span class="droptext">Power:<span style="width:500px;">What this robot uses for power.</span></span></b> Solar and Electrical <br>
+<b><span class="droptext">Full Recharge Rate:<span style="width:500px;">How long it takes this robot to recharge. (If applicable)</span></span></b> 6 hours if batteries are fully depleted. <br>
+<b>Color(s):</b> Black and Blue <br>
+
+<h2>Price: $2,500,000.00</h2>
+
+<br><p><font color="#FF0000" size="-1">**CAUTION: Do not allow the Immoveable Object to go head to head with the Unstoppable Force. Unknown conquences may occur.</font>
+
+
+<?php include ('footer.php'); ?>
