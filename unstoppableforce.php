f535fffb71f00aacff2f81a730a7d7af2b451679
Chris Zag <cezaglan@oakland.edu>
2012-11-13 13:22:06 -0500

    unstoppableforce.php
    My robot page, description is not complete yet

---
+++ unstoppableforce.php
@@ -0,0 +1,23 @@
+<?php include ('header.php'); ?>
+
+<h1>The Unstoppable Force</h1>
+<img src="http://www.secs.oakland.edu/~mjdawson/images/filler.jpg" height="300" width="250" border="0" alt="Immoveable Object" align="left">
+We are pleased to announce the Unstoppable Force, our most advanced assault robot to date. The Unstoppable Force features a mixture of advanced weaponry, self-shielding, and nanite technology. The Unstoppable Force has two forearm mo 
+<p>
+
+<h2>Product Specifications</h2>
+<b>Dimensions:</b> 96 inches tall x 36 inches wide (2.44 m x 0.91m)    			<br>
+<b><span class="droptext">Robot Mass:<span style="width:500px;">How much the robot weighs.</span></span></b> 713 lbs (322.4 kg)<br>
+<b>Operating temperature range:</b>	67 F to 89 F (19.4 C to 31.7 C)		<br>
+<b><span class="droptext">Frame:<span style="width:500px;">What the robot is made from.</span></span></b> Quadanium Steel <br>
+<b><span class="droptext">Class:<span style="width:500px;">The general type of robot.</span></span></b> Assault Bot <br>
+<b><span class="droptext">Power:<span style="width:500px;">What this robot uses for power.</span></span></b> Arch Reactor (primary) and Solar (secondary) <br>
+<b><span class="droptext">Full Recharge Rate:<span style="width:500px;">How long it takes this robot to recharge. (If applicable)</span></span></b> Self-powered unless there is a reactor malfunction <br>
+<b>Color(s):</b> Silver and Grey <br>
+
+<h2>Price: $5,000,000.00</h2>
+
+<br><p><font color="#FF0000" size="-1">**CAUTION: Do not allow the Unstoppable Force to go head to head with the Immovable Object. Unknown conquences may occur.</font>
+
+
+<?php include ('footer.php'); ?>
