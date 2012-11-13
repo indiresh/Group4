99898a5f588e99663c845b1a552d445b9d2cf77a
Alex Grodin <algrodin@oakland.edu>
2012-11-13 09:52:43 -0500

    Information

---
+++ About us.php
@@ -0,0 +1,25 @@
+<?php include ('header.php'); ?>
+
+<h1>About Us</h1>
+Welcome to Build-a-Bot!
+
+We’re a team of four engineers, each with our own unique robot architecture, 
+working constantly to create the best Build-a-Bot service in the nation. We 
+were randomly sitting together one day playing with robots, creating them and
+coming up with multiple ideas of kinds of robots. We came up with so many that 
+we needed to put it to good use. Hence, now have a company called Build-a-bot
+to share with all of you.
+<p>
+But instead of talking about us, let’s get to know you. Whether you have a 
+passion for robots, building them, or looking for something new to do, you 
+got to have knowledge about a Build-a-Bot service that will satisfy your 
+interests. It will also allow you to build excitement and focus on robots 
+in your free time. 
+<p>
+Build-a-Bot makes it easy to search through diverse robot products, read 
+updates about new products, learn about different parts and actions robots 
+do, and just generally explore them. You’ll love searching through the 
+variety of robots and it can make one’s life exciting! Think of using this 
+Build-a-Bot as your own personal robot platform. 
+
+<?php include ('footer.php'); ?>
---
+++ Rosie.php
@@ -0,0 +1,42 @@
+<?php include ('header.php'); ?>
+
+<h1>Rosie</h1>
+Rosie is the ultimate maid robot that moves quickly around a home doing 
+household chores such as cleaning, cooking dinner and washing dishes. 
+Rosie has the heart of human being, and a soothing, reassuring voice. 
+Rosie is the soft, cuddly robot that can be stored in your closet any time. 
+Rosie is a soft, cushioned, squishy robot so that it is child proof around 
+the house. This makes it safe for kids in case they run into Rosie accidently.
+Rosie’s soft grips also prevent breaking eggs or even wine glasses. There are 
+also touchable sensors on the fingertips to handle a variety of delicate objects 
+and can act like human hands. 
+<p>
+Rosie is a super-robot that wears a red cape, as well as an apron. She has a 
+button to press that allows you to choose the color of her cape daily. Rosie 
+has a human-like body that has a rotating head, arms, and legs. The bottoms of
+the legs are on wheels so that can move around the home. She also has 3-D 
+vision to always spot chores that need to be done. It allows her to recognize 
+people and be able to turn on an object, such as a microwave or coffee maker 
+when it senses it is about to be used. Generally, it is trained to mimic human 
+movements and apply them before the human can.Rosie’s purpose is to make your 
+life easier, and allow you to have more time to do other things. She will keep
+your house spot clean, prepare all kinds of things for you, and just overall 
+be the best maid you could ever have.
+<p>
+
+
+<h2>Product Specifications: </h2>
+
+Height: 4.3 feet 
+Weight: 121 pounds
+Made of silicone
+Rubber grippers pumped with air 
+Color layers for cape
+Sensory system
+Computer Brain
+Motor for movement
+Power: Battery – 10 hours, rechargeable
+
+<h2>Price: $1,300,000.00 </h2>
+
+<?php include ('footer.php'); ?>
