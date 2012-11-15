5febd522e36481b1d7e4f621fbdcdb7c0f4ea7bb
Alex Grodin <algrodin@oakland.edu>
2012-11-15 14:54:40 -0500

    Update techsupport.php

--- techsupport.php
+++ techsupport.php
@@ -4,6 +4,8 @@
 
 <p>
 <b>Q: My robot keeps assuring me, this is not the bot I am looking for. Why?</b><br>
+A. Your robot probably watched star wars while you were away and finds that part of the movie particularly hilarious.
+Erase your bot's memory starting from the day that this occurred and hide all of your star wars DVDs.
 
 <p>
 <b>Q. My robot is not working, what do I do?</b><br>
@@ -23,36 +25,47 @@ A. We think of a robot as something fully automatic.
 
 <p>
 <b>Q: If I have lost control of my robot, and it it currently rampaging a small village, what should I do?</b><br>
+A. Go to Japan. Find Godzilla. Ask him to come fight your robot. Sell tickets. Profit. (Numbered list)
 
 <p>
 <b>Q: How do I stop my robot from doing the robot?</b><br>
+A. If your robot keeps doing the robot, you can remove its arms. Your robot will retain up to 88% functionality without them.
 
 <p>
-<b>Q: My robot says it needs more cookies, and I tried feeding him chocolate chip, and it didnt work; what should I do?</b><br>
+<b>Q: My robot says it needs more cookies, and I tried feeding him chocolate chip, and it didnt work, what should I do?</b><br>
+A. Robots do not eat cookies. You are obviously doing something wrong. Please stop shoving cookies into your poor robot's orifices.
 
 <p>
 <b>Q: My robot wants a heart, any plans on implementing those in the future?</b><br>
+A. You can purchase the robot heart accessory from our website. It's only 3 easy payments of $19.99.
 
 <p>
 <b>Q: My robot is talking in beeps. How do I fix that?</b><br>
+A. Change the language setting in the robot's mouth from "binary" to "English".
 
 <p>
 <b>Q: My robot keeps staring up into the sky asking if it can go home. What's wrong with it?</b><br>
+A. Your robot is obviously miserable and wants to go to robot heaven. Switch off its power and feel ashamed. You are a terrible robot owner. Please never buy robots from us again.
 
 <p>
 <b>Q: My bot keeps digging up all my plants and putting them into my shoes. How do I fix this?</b><br>
+A. It appears that your robot is in WALL-E mode. Preform a soft-reset and keep your plants elevated in the future.
 
 <p>
 <b>Q: When I send my bot away to do tasks, it always replies with "I'll be back."</b><br>
+A. Your robot is the terminator. Dispose of your robot immediately.
 
 <p>
 <b>Q: When I ask my robot for advice, it tells me to use the Schwartz. What's the Shwartz?</b><br>
+A. Please refer to the first question, but hide your copy of spaceballs, instead.
 
 <p>
 <b>Q: My robot sits and talks to my car. Why is my robot trying to talk to my car?</b><br>
+A. 
 
 <p>
 <b>Q: My robot keeps taking bites out of anything metal in my house. Why?</b><br>
+A. 
 
 <br><br><b>If you have more questions that have not been answered, please go to the <a href="http://www.secs.oakland.edu/~mjdawson/contactus.php">Contact Us</a> page for further assistance.</b>
 
