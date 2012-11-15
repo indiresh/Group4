701f57b39bb22be34ba25fd7999e424cd1949c3d
Alex Grodin <algrodin@oakland.edu>
2012-11-15 13:42:25 -0500

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -4,20 +4,20 @@
 
 <img src="http://cache.boston.com/universal/site_graphics/blogs/
 bigpicture/robots_03_04/r01_17590631.jpg" height="693" 
-width="990" border="0" alt="Immoveable Object" align="center">
+width="990" border="0" alt="Robot" align="center">
 
 <a href="http://www.boston.com/bigpicture/2009/03/robots.html">
 
 Welcome to Build-a-Bot!
 
-We’re a team of four engineers, each with our own unique robot architecture, 
+Weï¿½re a team of four engineers, each with our own unique robot architecture, 
 working constantly to create the best Build-a-Bot service in the nation. We 
 were randomly sitting together one day playing with robots, creating them and
 coming up with multiple ideas of kinds of robots. We came up with so many that 
 we needed to put it to good use. Hence, now have a company called Build-a-bot
 to share with all of you.
 <p>
-But instead of talking about us, let’s get to know you. Whether you have a 
+But instead of talking about us, letï¿½s get to know you. Whether you have a 
 passion for robots, building them, or looking for something new to do, you 
 got to have knowledge about a Build-a-Bot service that will satisfy your 
 interests. It will also allow you to build excitement and focus on robots 
@@ -25,8 +25,8 @@ in your free time.
 <p>
 Build-a-Bot makes it easy to search through diverse robot products, read 
 updates about new products, learn about different parts and actions robots 
-do, and just generally explore them. You’ll love searching through the 
-variety of robots and it can make one’s life exciting! Think of using this 
+do, and just generally explore them. Youï¿½ll love searching through the 
+variety of robots and it can make oneï¿½s life exciting! Think of using this 
 Build-a-Bot as your own personal robot platform. 
 
 <?php include ('footer.php'); ?>
