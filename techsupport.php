c4e777dff12b924dc4785dfffd7db76432cc703d
Maggie Dawson <mjdawson@oakland.edu>
2012-11-15 13:59:07 -0500

    techsupport.php
    Chris and I came up with some tech support questions - most of them are
    from geeky movies involving robots. You just need to come up with
    answers! I also fixed some formatting.

--- techsupport.php
+++ techsupport.php
@@ -1,20 +1,54 @@
 <?php include ('header.php'); ?>
 
-<h1>FAQ</h1>
+<h1>Tech Support</h1>
 
-Q. My robot is not working, what do I do?
+<p>
+<b>Q: My robot keeps assuring me, this is not the bot I am looking for. Why?</b><br>
+
+<p>
+<b>Q. My robot is not working, what do I do?</b><br>
 A. Try turning it off and then back on, it restarts the system.
 
-Q. What if I forgot my password?
+<p>
+<b>Q. What if I forgot my password?</b><br>
 A. Click on "Forgot Password" and type in your email.
 
-Q. How do I keep updated with all the new products coming out?
+<p>
+<b>Q. How do I keep updated with all the new products coming out?</b><br>
 A. Go to our blog, it is updated with news about the products.
 
-Q. Why is it called a robot?
+<p>
+<b>Q. Why is it called a robot?</b><br>
 A. We think of a robot as something fully automatic.
 
-If you have more questions that have not been answered, please go to the 
-Contact Us Page for further assistance.
+<p>
+<b>Q: If I have lost control of my robot, and it it currently rampaging a small village, what should I do?</b><br>
+
+<p>
+<b>Q: How do I stop my robot from doing the robot?</b><br>
+
+<p>
+<b>Q: My robot says it needs more cookies, and I tried feeding him chocolate chip, and it didnt work; what should I do?</b><br>
+
+<p>
+<b>Q: My robot wants a heart, any plans on implementing those in the future?</b><br>
+
+<p>
+<b>Q: My robot is talking in beeps. How do I fix that?</b><br>
+
+<p>
+<b>Q: My robot keeps staring up into the sky asking if it can go home. What's wrong with it?</b><br>
+
+<p>
+<b>Q: My bot keeps digging up all my plants and putting them into my shoes. How do I fix this?</b><br>
+
+<p>
+<b>Q: When I send my bot away to do tasks, it always replies with "I'll be back."</b><br>
+
+<p>
+<b>Q: When I ask my robot for advice, it tells me to use the Schwartz. What's the Shwartz?</b><br>
+
+
+<br><br><b>If you have more questions that have not been answered, please go to the <a href="http://www.secs.oakland.edu/~mjdawson/contactus.php">Contact Us</a> page for further assistance.</b>
 
 <?php include ('footer.php'); ?>
