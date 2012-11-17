78b3e4e23a8c4f427160918dc581b5c2bb284e44
Alex Grodin <algrodin@oakland.edu>
2012-11-17 03:33:31 -0500

    Formatting for Rosie

--- Rosie.php
+++ Rosie.php
@@ -1,6 +1,8 @@
 <?php include ('header.php'); ?>
 
 <h1>Rosie</h1>
+<img src="http://www.secs.oakland.edu/~mjdawson/images/filler.jpg" height="300" 
+width="250" border="0" alt="Immoveable Object" align="left">
 Rosie is the ultimate maid robot that moves quickly around a home doing 
 household chores such as cleaning, cooking dinner and washing dishes. 
 Rosie has the heart of human being, and a soothing, reassuring voice. 
@@ -11,32 +13,30 @@ Rosie’s soft grips also prevent breaking eggs or even wine glasses. There are
 also touchable sensors on the fingertips to handle a variety of delicate objects 
 and can act like human hands. 
 <p>
-Rosie is a super-robot that wears a red cape, as well as an apron. She has a 
-button to press that allows you to choose the color of her cape daily. Rosie 
-has a human-like body that has a rotating head, arms, and legs. The bottoms of
-the legs are on wheels so that can move around the home. She also has 3-D 
+Rosie has a human-like body that has a rotating head, arms, and legs. She has 3-D 
 vision to always spot chores that need to be done. It allows her to recognize 
 people and be able to turn on an object, such as a microwave or coffee maker 
 when it senses it is about to be used. Generally, it is trained to mimic human 
-movements and apply them before the human can.Rosie’s purpose is to make your 
+movements and apply them before the human can. Rosie’s purpose is to make your 
 life easier, and allow you to have more time to do other things. She will keep
 your house spot clean, prepare all kinds of things for you, and just overall 
 be the best maid you could ever have.
 <p>
 
-
 <h2>Product Specifications: </h2>
-
-Height: 4.3 feet 
-Weight: 121 pounds
-Made of silicone
-Rubber grippers pumped with air 
-Color layers for cape
-Sensory system
-Computer Brain
-Motor for movement
-Power: Battery – 10 hours, rechargeable
+<b>Height:</b> 4.3 feet <br>
+<b><span class="droptext">Robot Mass:<span style="width:500px;">How much 
+the robot weighs.</span></span></b> 121 lbs <br>
+<b><span class="droptext">Frame:<span style="width:500px;">What the robot 
+is made from.</span></span></b> Silicone <br>
+<b>Special:</b> Rubber grippers pumped with air, Sensory system, Computer Brain, 
+Motor for movement<br>
+<b><span class="droptext">Power:<span style="width:500px;">How long it 
+takes this robot to last. (If applicable)</span></span></b> Battery – 10 hours, 
+rechargeable. <br>
 
 <h2>Price: $1,300,000.00 </h2>
+
+<br><p><font color="#FF0000" size="-1">**CAUTION: May be too cuddly, do not get too attached.</font>
 
 <?php include ('footer.php'); ?>
