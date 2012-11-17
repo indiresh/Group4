a8406089427a3cf6e1957da8630515101641bbeb
Alex Grodin <algrodin@oakland.edu>
2012-11-17 03:40:32 -0500

    formatting

--- Rosie.php
+++ Rosie.php
@@ -29,7 +29,7 @@ be the best maid you could ever have.
 the robot weighs.</span></span></b> 121 lbs <br>
 <b><span class="droptext">Frame:<span style="width:500px;">What the robot 
 is made from.</span></span></b> Silicone <br>
-<b>Special:</b> Rubber grippers pumped with air, Sensory system, Computer Brain, 
+<b>Special:</b> Rubber grippers pumped with air <br> Sensory system <br> Computer Brain <br>
 Motor for movement<br>
 <b><span class="droptext">Power:<span style="width:500px;">How long it 
 takes this robot to last. (If applicable)</span></span></b> Battery – 10 hours, 
