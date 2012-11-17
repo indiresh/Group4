8cb7b35f3e1b99c37c19200ccae3a699f7e62759
Chris Zag <cezaglan@oakland.edu>
2012-11-17 15:41:02 -0500

    aboutus.php
    Fixed a few grammar problems and fixed the apostrophes to show correctly

--- aboutus.php
+++ aboutus.php
@@ -8,23 +8,23 @@ width="396" border="0" alt="robot" align="left">
 
 Welcome to Build-a-Bot!
 
-We�re a team of four engineers, each with our own unique robot architecture, 
+We're a team of four engineers, each with our own unique robot architecture, 
 working constantly to create the best Build-a-Bot service in the nation. We 
 were randomly sitting together one day playing with robots, creating them and
-coming up with multiple ideas of kinds of robots. We came up with so many that 
-we needed to put it to good use. Hence, now have a company called Build-a-bot
+coming up with multiple ideas for robots. We came up with so many that 
+we decided to put it to good use. Hence, we now have a company called Build-a-bot
 to share with all of you.
 <p>
-But instead of talking about us, let�s get to know you. Whether you have a 
-passion for robots, building them, or looking for something new to do, you 
-got to have knowledge about a Build-a-Bot service that will satisfy your 
-interests. It will also allow you to build excitement and focus on robots 
+But instead of talking about us, let's get to know you. Whether you have a 
+passion for robots, enjoy building them, or are looking for something new to do, 
+having knowledge about our Build-a-Bot service will satisfy your interests. 
+It will give you the ability to build, program, and focus on robots 
 in your free time. 
 <p>
 Build-a-Bot makes it easy to search through diverse robot products, read 
-updates about new products, learn about different parts and actions robots 
-do, and just generally explore them. You�ll love searching through the 
-variety of robots and it can make one�s life exciting! Think of using this 
+updates about new products, learn about different parts, provides tutorial about the actions 
+robots are capable of, or just generally explore them. You'll love searching through the 
+variety of robots and it can make one's life exciting! Think of using this 
 Build-a-Bot as your own personal robot platform. 
 
 <?php include ('footer.php'); ?>
