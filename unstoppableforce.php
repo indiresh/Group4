e5b659c03c44a56e57a1f18c74ca159d6c9c5d0c
Chris Zag <cezaglan@oakland.edu>
2012-11-20 13:41:45 -0500

    unstoppableforce.php
    added the robot image to the page

--- unstoppableforce.php
+++ unstoppableforce.php
@@ -1,7 +1,7 @@
 <?php include ('header.php'); ?>
 
 <h1>The Unstoppable Force</h1>
-<img src="http://www.secs.oakland.edu/~mjdawson/images/filler.jpg" height="300" width="250" border="0" alt="Immoveable Object" align="left">
+<img src="http://www.secs.oakland.edu/~mjdawson/images/unstoppableforce.jpg" height="300" width="250" border="0" alt="Immoveable Object" align="left">
 We are pleased to announce the Unstoppable Force, our most advanced assault robot to date. The Unstoppable Force features a mixture of advanced weaponry and nanite technology. The Unstoppable Force has two forearm mounted ion cannons that are retractable into the robot. To complete its arsenal, two rotating miniature missile silos are attached to the robot's shoulders. The Unstoppable Force uses our cutting-edge targeting system, Skyweb. Our designers also implemented our first generation nanite repair module into the design of the robot. The nanites can repair most damage incurred by the robot unless it results in a system malfunction or shutdown.  
 <p>
 The Unstoppable Force is a great way to keep your enemies, archrivals, bad drivers, other super villains, or those bratty children on your front lawn in line. It shattered all previous records achieved on our assault robot testing course by jaw dropping margins. This robot is a must have for all aspiring world conquerors or if you just like playing world-sized Risk on the weekends. The Unstoppable Force is guaranteed to induce maniacal laughter to its owners upon witnessing its abilities in action or we’ll give you your money back (really the check is in the mail). 
