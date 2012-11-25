bc9f33d4ff83fe78989e5825f71a721aa7bc202f
Chris Zag <cezaglan@oakland.edu>
2012-11-25 17:26:26 -0500

    join_form.php
    Changed formatting to match other forms. Changed input type for
    email_address to email so form checks if input is a valid email address

--- join_form.php
+++ join_form.php
@@ -3,23 +3,23 @@
 <form name="form1" method="post" action="register.php">
   <table width="100%" border="0" cellpadding="4" cellspacing="0">
     <tr> 
-      <td width="24%" align="left" valign="top">First Name</td>
+      <td width="24%" align="right" valign="top">First Name:</td>
       <td width="76%"><input name="first_name" type="text" id="first_name2" value="<? echo $first_name; ?>"></td>
     </tr>
     <tr> 
-      <td align="left" valign="top">Last Name</td>
+      <td align="right" valign="top">Last Name:</td>
       <td><input name="last_name" type="text" id="last_name" value="<? echo $last_name; ?>"></td>
     </tr>
     <tr> 
-      <td align="left" valign="top">Email Address</td>
-      <td><input name="email_address" type="text" id="email_address" value="<? echo $email_address; ?>"></td>
+      <td align="right" valign="top">Email Address:</td>
+      <td><input name="email_address" type="email" id="email_address" value="<? echo $email_address; ?>"></td>
     </tr>
     <tr> 
-      <td align="left" valign="top">Address:</td>
+      <td align="right" valign="top">Address:</td>
       <td><textarea name="address" id="address"><? echo $address; ?></textarea></td>
     </tr>
     <tr> 
-      <td align="left" valign="top">&nbsp;</td>
+      <td align="right" valign="top">&nbsp;</td>
       <td><input type="submit" name="Submit" value="Join Now!"></td>
     </tr>
   </table>
