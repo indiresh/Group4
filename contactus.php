fe0bff5ebfe5f42b667f6a82fc9d2e5ab708ad0b
Chris Zag <cezaglan@oakland.edu>
2012-11-25 19:29:38 -0500

    contactus.php
    Beginning and end of file got screwed up some how. Fixed them to be
    correct

--- contactus.php
+++ contactus.php
@@ -1,6 +1,3 @@
-<?php require_once 'includes\connection.php'; ?>
-<?php require_once 'includes\functions.php'; ?>
-<?php find_selected_page(); ?>
 <?php include ('header.php'); ?>
 
 <form name="contactform" method="post" action="send_email.php">
@@ -45,7 +42,7 @@
 		<option value="Other">Other</option></td>
   </tr>
   <tr>
-  	<td align=right valign=top>Comments:</td>
+    <td align=right valign=top>Comments:</td>
 	<td><textarea name="comments" rows=8 cols=50></textarea></td>
   </tr>	
   <tr>
@@ -53,13 +50,7 @@
 	<td><input type="submit" name="Submit" value="Submit" class="button"><INPUT TYPE="reset" value="Clear" class="button"></td>	
   </tr>
 
-<!-- End Content -->
-</td>
-</tr>
 </table>
 </form> 	
-</div>
 
-
-
-
+<?php include ('footer.php'); ?>
