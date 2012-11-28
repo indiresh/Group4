2aa8f585d0e1cf5b7671b3fde9093f110a4f1a45
Chris Zag <cezaglan@oakland.edu>
2012-11-27 21:01:43 -0500

    member\index.php
    added more news announcements

--- members/index.php
+++ members/index.php
@@ -1,5 +1,25 @@
 <?php include ('header.php'); ?>
 
+<h2>October 21</h2>
+<div class="comment">
+<h3>2012 Homemade Robot Contest Winner Choosen</h3>
+After much debate here at Build-A-Bot, we have finally picked a winner for our annual Homemade Robot Contest. This year’s contest was the closest to date, with many recounts and even a few brawls breaking out between opposing camps. The 2012 winner comes to us from The Orkin Man. The Orkin Man’s robot, the Bugginator, was a modified Roomba. His robot main purpose is the complete and utter extermination of bugs from the household. Featuring a set of eight spider-like legs for great horizontal and vertical mobility, this bug terminator bot uses motion and heat sensing technology to locate unwanted insects. After acquiring its target the bot quickly swoops in to disorient its victim with differing sound frequencies and then zaps the target with a laser.  The Bugginator also features a collection system for cleaning up fried bug corpses. The easy to use interface and customization carried the Bugginator to victory in this year’s contest.
+</div>
+
+<h2>October 12</h2>
+<div class="comment">
+<h3>Ultimate Defensive Robot Announced</h3>
+We are pleased to announce that one of our most experienced and successful design teams, S.H.I.E.L.D., will be undertaking a new project. The team is led by distinguished robot designer, Nick Fury. Mr. Fury has long said the main objective of his team is to protect the world. In announcing S.H.I.E.L.D.’s new undertaking, Mr. Fury commented, “The invasion of the Chitauri aliens made it clear that Earth can be in danger from foes not even known to us. We must be prepared at all times to defend ourselves. Therefore, my group has decided to create a bot that can provide unprecedented protection to its owner.”
+<p>
+Mr. Fury was very sparse on providing more details about the robot other than hinting that a breakthrough in metallurgy and a revamped defensive software package will be main components of the new bot. When asked if their new defensive robot could withstand the Unstoppable Force, Mr. Fury scoffed, “People, please. We all know what happened to the Juggernaut. He claimed to be unstoppable and we all know what happened to him. Nothing is unstoppable.” We, here at Build-A-Bot, wait in eager anticipation of S.H.I.E.L.D.’s completion of their latest project; rumors of a show match between their creation and the Unstoppable Force are already running rampant. 
+</div>
+
+<h2>October 9</h2>
+<div class="comment">
+<h3>The Unstoppable Force Now Available</h3>
+Today a new robot has become available for purhcase! The Unstoppable Force has been released by the Legion of Doom, our newest development team. The Unstoppable Force features a host of advanced technology. Most notably, ion cannons, a state-of-the-art weapon system newly developed by Legion of Doom members specifically for this bot; our first generation nanite repair module; and Skyweb, our most cutting-edge targeting system. Mr. Luther, leader of the Legion of Doom, was proud of the final product that resulted from the many hours of research and development done by his team over the past few weeks. Mr. Luther commented that “this robot will revolutionize the world.” We at Build-A-Bot mimic Mr. Luther’s sentiments and believe The Unstoppable Force makes a great addition to the Build-A-Bot robot family. Be sure to check The Unstoppable Force product page for more information. Place your orders now!
+</div>
+
 <h2>September 24</h2>
 <div class="comment">
 <h3>OS Patch 1.4.3 Released</h3>
