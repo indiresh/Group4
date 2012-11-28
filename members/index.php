ebdec496b290ff16fbbe7ca57cff433fd372ad47
Alex Grodin <algrodin@oakland.edu>
2012-11-28 08:30:06 -0500

    update home page

--- members/index.php
+++ members/index.php
@@ -1,23 +1,57 @@
 <?php include ('header.php'); ?>
 
+<h2>November 27</h2>
+<div class="comment">
+<h3>Rosie Now Available</h3>
+Today a new robot has become available for purchase! Rosie has been released by the Neat Freaks, our cleanliest and most obsessive compulsive development team. Rosie is truly a robot to end the war against filth. Rosie features a host of sensors and protocols developed specifically to keep any area spotless. She works with flawless precision and efficiency. Her unique rubber grippers allow Rosie to handle the most delicate of tasks. Rosie also includes an unprecedented humanistic artificial intelligence allowing her to be the ultimate service bot; whether you need something cleaned, a chore completed, or just a companion, Rosie is well equipped to do it all. As promised, Rosie is child-safe with a soft-squishy exterior and easy-to-use with voice command activation. Mister Clean was all smiles while introducing his team’s robot. He even partook in some trash talk, “Take that you dirty, grimy filth. Rosie is going to clean you into submission.” We, at Build-A-Bot, are highly impressed with the Neat Freaks’ work and believe Rosie will revolutionize life as we know it. Build-A-Bot is delighted to welcome Rosie into the Build-A-Bot robot family. Be sure to check the Rosie product page for more information. Place your orders now!
+</div>
+
+<h2>November 14</h2>
+<div class="comment">
+<h3>OS Patch 1.4.4 Released</h3>
+Today we released a patch for our robots' operating system, Nutz'n'Boltz. This patch fixes a few minor bugs reported by our users. Please be sure to restart your robot so that it may download the update. Please note that the patch will cause your robot to take a few extra minutes to boot up. 
+<p>
+<u>Patch Notes:</u>
+<p>
+-Fixed an issue that caused some robots to say “whelmed” and never specifying if they meant “underwhelmed” or “overwhelmed”.
+<p>
+-Fixed a known bug that would cause some robots to communicate in Pig Latin
+<p>
+-Fixed a reported bug that caused some bots to tie themselves into knots while playing Twister
+</div>
+
+<h2>November 5</h2>
+<div class="comment">
+<h3>Never Have to Clean Again Robot Announced</h3>
+We are pleased to announce that the most hygienic and obsessive compulsive group of the Build-A-Bot family, the Neat Freaks, will be starting a new project. The Neat Freaks have long waged a war against dirt, grime, and dirty clothes; a war that recently the filth has been gaining ground in. However, the Neat Freaks believe they have come up with a weapon that will win them the war once and for all. They have code named their new endeavor, the Manhattan Project. Team leader, Mister Clean, in his press release said, “The results of our undertaking will be a knockout blow that filth will never recover from. This bot will change life as we know it.”
+<p>
+Mister Clean assured that the project will meet all Neat Freak standards of being child-safe, easy-to-use, able to tackle any job, and amazing. The Neat Freaks have long been developing a humanistic artificial intelligence; speculation has arisen that perhaps they will finally implement it. The only concrete detail leaked is that the robot will feature rubber fingers, an interesting tidbit. We’ll just have to wait and see what the final product holds in store for us when it is delivered, but Build-A-Bot is excited to see if Mister Clean and his Neat Freaks can deliver on their bold promises.
+</div>
+
+<h2>October 30</h2>
+<div class="comment">
+<h3>Immovable Object Now Available</h3>
+Today a new robot has become available for purchase! The Immovable Object has been released by S.H.I.E.L.D., our most experienced and successful development team. The Immovable Object features a host of cutting edge technology. This robot has revolutionized defensive robotics! The robot is encased in a shell of vibranium, a metallurgic invention created by S.H.I.E.L.D. Vibranium absorbs any vibration allowing the robot to absorb massive hits without so much as even a scratch. S.H.I.E.L.D.’s “Guard Dog” software comes pre-loaded into the Immovable Object. “Guard Dog” is a groundbreaking protection program; it constantly monitors the feedback the robot provides about its surroundings and calculates potential hazards. Should something register at a high enough danger level, the Immovable Object warns its owner. Mr. Fury was beaming with pride over the bot built by his team; he even commented, “I’d like to see those Legion of Doom noobs try to even put a dent on our creation with that tinker toy of theirs.” Build-A-Bot remains impartial over this matter and are happy to have both bots in our Build-A-Bot robot family. Be sure to check the Immovable Object product page for more information. Place your orders now!
+</div>
+
 <h2>October 21</h2>
 <div class="comment">
 <h3>2012 Homemade Robot Contest Winner Choosen</h3>
-After much debate here at Build-A-Bot, we have finally picked a winner for our annual Homemade Robot Contest. This year�s contest was the closest to date, with many recounts and even a few brawls breaking out between opposing camps. The 2012 winner comes to us from The Orkin Man. The Orkin Man�s robot, the Bugginator, was a modified Roomba. His robot main purpose is the complete and utter extermination of bugs from the household. Featuring a set of eight spider-like legs for great horizontal and vertical mobility, this bug terminator bot uses motion and heat sensing technology to locate unwanted insects. After acquiring its target the bot quickly swoops in to disorient its victim with differing sound frequencies and then zaps the target with a laser.  The Bugginator also features a collection system for cleaning up fried bug corpses. The easy to use interface and customization carried the Bugginator to victory in this year�s contest.
+After much debate here at Build-A-Bot, we have finally picked a winner for our annual Homemade Robot Contest. This year’s contest was the closest to date, with many recounts and even a few brawls breaking out between opposing camps. The 2012 winner comes to us from The Orkin Man. The Orkin Man’s robot, the Bugginator, was a modified Roomba. His robot main purpose is the complete and utter extermination of bugs from the household. Featuring a set of eight spider-like legs for great horizontal and vertical mobility, this bug terminator bot uses motion and heat sensing technology to locate unwanted insects. After acquiring its target the bot quickly swoops in to disorient its victim with differing sound frequencies and then zaps the target with a laser.  The Bugginator also features a collection system for cleaning up fried bug corpses. The easy to use interface and customization carried the Bugginator to victory in this year’s contest.
 </div>
 
 <h2>October 12</h2>
 <div class="comment">
 <h3>Ultimate Defensive Robot Announced</h3>
-We are pleased to announce that one of our most experienced and successful design teams, S.H.I.E.L.D., will be undertaking a new project. The team is led by distinguished robot designer, Nick Fury. Mr. Fury has long said the main objective of his team is to protect the world. In announcing S.H.I.E.L.D.�s new undertaking, Mr. Fury commented, �The invasion of the Chitauri aliens made it clear that Earth can be in danger from foes not even known to us. We must be prepared at all times to defend ourselves. Therefore, my group has decided to create a bot that can provide unprecedented protection to its owner.�
+We are pleased to announce that one of our most experienced and successful design teams, S.H.I.E.L.D., will be undertaking a new project. The team is led by distinguished robot designer, Nick Fury. Mr. Fury has long said the main objective of his team is to protect the world. In announcing S.H.I.E.L.D.’s new undertaking, Mr. Fury commented, “The invasion of the Chitauri aliens made it clear that Earth can be in danger from foes not even known to us. We must be prepared at all times to defend ourselves. Therefore, my group has decided to create a bot that can provide unprecedented protection to its owner.”
 <p>
-Mr. Fury was very sparse on providing more details about the robot other than hinting that a breakthrough in metallurgy and a revamped defensive software package will be main components of the new bot. When asked if their new defensive robot could withstand the Unstoppable Force, Mr. Fury scoffed, �People, please. We all know what happened to the Juggernaut. He claimed to be unstoppable and we all know what happened to him. Nothing is unstoppable.� We, here at Build-A-Bot, wait in eager anticipation of S.H.I.E.L.D.�s completion of their latest project; rumors of a show match between their creation and the Unstoppable Force are already running rampant. 
+Mr. Fury was very sparse on providing more details about the robot other than hinting that a breakthrough in metallurgy and a revamped defensive software package will be main components of the new bot. When asked if their new defensive robot could withstand the Unstoppable Force, Mr. Fury scoffed, “People, please. We all know what happened to the Juggernaut. He claimed to be unstoppable and we all know what happened to him. Nothing is unstoppable.” We, here at Build-A-Bot, wait in eager anticipation of S.H.I.E.L.D.’s completion of their latest project; rumors of a show match between their creation and the Unstoppable Force are already running rampant. 
 </div>
 
 <h2>October 9</h2>
 <div class="comment">
 <h3>The Unstoppable Force Now Available</h3>
-Today a new robot has become available for purhcase! The Unstoppable Force has been released by the Legion of Doom, our newest development team. The Unstoppable Force features a host of advanced technology. Most notably, ion cannons, a state-of-the-art weapon system newly developed by Legion of Doom members specifically for this bot; our first generation nanite repair module; and Skyweb, our most cutting-edge targeting system. Mr. Luther, leader of the Legion of Doom, was proud of the final product that resulted from the many hours of research and development done by his team over the past few weeks. Mr. Luther commented that �this robot will revolutionize the world.� We at Build-A-Bot mimic Mr. Luther�s sentiments and believe The Unstoppable Force makes a great addition to the Build-A-Bot robot family. Be sure to check The Unstoppable Force product page for more information. Place your orders now!
+Today a new robot has become available for purchase! The Unstoppable Force has been released by the Legion of Doom, our newest development team. The Unstoppable Force features a host of advanced technology. Most notably, ion cannons, a state-of-the-art weapon system newly developed by Legion of Doom members specifically for this bot; our first generation nanite repair module; and Skyweb, our most cutting-edge targeting system. Mr. Luther, leader of the Legion of Doom, was proud of the final product that resulted from the many hours of research and development done by his team over the past few weeks. Mr. Luther commented that “this robot will revolutionize the world.” We, at Build-A-Bot, mimic Mr. Luther’s sentiments and believe The Unstoppable Force makes a great addition to the Build-A-Bot robot family. Be sure to check The Unstoppable Force product page for more information. Place your orders now!
 </div>
 
 <h2>September 24</h2>
@@ -37,9 +71,9 @@ Today we released a patch for our robots' operating system, Nutz'n'Boltz. This p
 <h2>September 19</h2>
 <div class="comment">
 <h3>Legion of Doom Joins Build-A-Bot</h3>
-We are pleased to announce an addition to our development department! Over the past few months, we have been in talks with a brilliant group of designers led by Lex Luther. Mr. Luther�s group, known as the Legion of Doom, has been on the cutting edge of weapons development for the past few years. We, here at Build-A-Bot, have been impressed by their continued dedication to their goals and resiliency when met with failure. 
+We are pleased to announce an addition to our development department! Over the past few months, we have been in talks with a brilliant group of designers led by Lex Luther. Mr. Luther’s group, known as the Legion of Doom, has been on the cutting edge of weapons development for the past few years. We, here at Build-A-Bot, have been impressed by their continued dedication to their goals and resiliency when met with failure. 
 <p>
-Legion of Doom members were enthusiastic to join the Build-A-Bot family and gain access to our advanced robotic models. Upon arrival at the Build-A-Bot development facility, they quickly locked themselves into their lab and have been hard at work since. While Mr. Luther has been tight lipped about his team�s first project, he did let slip that he planned on �developing a force to take over the world.� While Build-A-Bot does not condone world-take over, we are eager to see what results from the integration of Build-A-Bot robot technology with Legion of Doom weaponry.
+Legion of Doom members were enthusiastic to join the Build-A-Bot family and gain access to our advanced robotic models. Upon arrival at the Build-A-Bot development facility, they quickly locked themselves into their lab and have been hard at work since. While Mr. Luther has been tight lipped about his team’s first project, he did let slip that he planned on “developing a force to take over the world.” While Build-A-Bot does not condone world-take over, we are eager to see what results from the integration of Build-A-Bot robot technology with Legion of Doom weaponry.
 <p>
 </div>
 
