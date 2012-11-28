0984823df4bf9842667fe357df23b1dd7f28abdb
Alex Grodin <algrodin@oakland.edu>
2012-11-28 10:12:30 -0500

    send order form correct?

--- send_order.php
+++ send_order.php
@@ -1,67 +1,17 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Built-a-Bot Robotics</title>
+<?php include ('header.php'); ?>
+<?php
+$username = $_POST['username'];
+$phone = $_POST['phone'];
+$robot = $_POST['robot'];
+$quantity = $_POST['quantity'];
+$payment = $_POST['payment'];
+$accept = $_POST['accept'];
+$formcontent=" From: $name \n Phone: $phone \n Robot(s): $robot \n Quantity: $quantity \n Payment Method: $payment \n Accept Terms: $accept";
+$recipient = "order@build-a-botrobotics.com";
+$subject = "Build-A-Bot";
+$mailheader = "From: $username \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error! Please go back and fill in all information.");
+echo "Thank you for purchasing our products! We will get back to you as soon as the order form has been processed.";
+?>
 
-<link rel="stylesheet" type="text/css" href="http://www.secs.oakland.edu/~mjdawson/css/main.css">
-<link rel="stylesheet" type="text/css" href="http://www.secs.oakland.edu/~mjdawson/menu/menu2.css">
-
-<script type="text/javascript" src="http://www.secs.oakland.edu/~mjdawson/menu/jQuery.js"></script>
-<script type="text/javascript" src="http://www.secs.oakland.edu/~mjdawson/menu/jquery.easing.1.3.js"></script>
-<script type="text/javascript" src="http://www.secs.oakland.edu/~mjdawson/menu/menu.js"></script>
-<script type="text/javascript" src="http://www.secs.oakland.edu/~mjdawson/jquery.shorten.1.0.js"></script>
-</head>
-<body>
-
-<!-- Cart Bar -->
-<div class="cart">
-		<table width="200" border="0" cellspacing="0" cellpadding="0" align="right">
-  		<tr>
-    	<td align="center"><a href="http://www.secs.oakland.edu/~mjdawson/login_form.php">Sign In</a> | </td>
-    	<td align="center"><a href="http://www.secs.oakland.edu/~mjdawson/join_form.php">Sign Up</a></td>
-    	<td align="center"><a href="http://www.secs.oakland.edu/~mjdawson/order_form.php">Order Form</a> | </td>
-  		</tr>
-		</table>
-</div>
-
-</br>
-</br>
-<img src="http://www.secs.oakland.edu/~mjdawson/images/logo.png" height="50" width="350" border="0" alt="logo" align="left">
-
-<!-- Navagation Bar -->
-<div class="menu">
- <ul class="nav">
-	<div id = "navLeft"></div>
-	<div id = "navRight"></div>
-	<li class = "mainmenu"><a href="http://www.secs.oakland.edu/~mjdawson/index.php">Home</a></li>
-            <li class = "submenu"><a href="#"><span></span>Products</a>
-			<ul>
-				<li><a href="http://www.secs.oakland.edu/~mjdawson/immoveableobject.php">Immoveable Object</a></li>
-				<li><a href="http://www.secs.oakland.edu/~mjdawson/unstoppableforce.php">Unstoppable Force</a></li>
-				<li><a href="http://www.secs.oakland.edu/~mjdawson/rosie.php">Rosie</a></li>
-				<li><a href="http://www.secs.oakland.edu/~mjdawson/bluethunder.php">Blue Thunder</a></li>
-			</ul></li>
-            <li class = "mainmenu"><a href="http://www.secs.oakland.edu/~mjdawson/aboutus.php">About Us</a></li>
-        <li class = "mainmenu"><a href="http://www.secs.oakland.edu/~mjdawson/techsupport.php">Tech Support</a></li>
-		<li class = "mainmenu"><a href="http://www.secs.oakland.edu/~mjdawson/contactus.php">Contact</a></li>
-		<li class = "mainmenu"><a href="#"></a></li>
- </ul>
-</div>
-
-
-
-<!-- Content -->
-<div class="content">
-<table width="98%" align="center">
-<tr><td>Thank you for purchasing our products! We will get back to you as the order form has been processed.
-</td>
-</tr>
-</table>	
-</div>
-
-
-
-
-</body>
-</html>
+<?php include ('footer.php'); ?>
