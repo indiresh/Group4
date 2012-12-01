586b3492b0ba53ebe57c7141cbc4ec36909a50f6
Chris Zag <cezaglan@oakland.edu>
2012-12-01 16:09:11 -0500

    techsupport.php
    Fixed a few grammar things and fixed the formatting a bit

--- techsupport.php
+++ techsupport.php
@@ -5,7 +5,7 @@
 <p>
 <b>Q: My robot keeps assuring me, this is not the bot I am looking for. Why?</b><br>
 A. Your robot probably watched star wars while you were away and finds that part of the movie particularly hilarious.
-Erase your bot's memory starting from the day that this occurred and hide all of your star wars DVDs.
+Erase your bot's memory starting from the day that this occurred and hide all of your Star Wars DVDs.
 
 <p>
 <b>Q. My robot is not working, what do I do?</b><br>
@@ -25,7 +25,7 @@ A. We think of a robot as something fully automatic.
 
 <p>
 <b>Q: If I have lost control of my robot, and it it currently rampaging a small village, what should I do?</b><br>
-A. 1) Go to Japan. <br> 2) Find Godzilla. <br> 3) Ask him to come fight your robot. <br> 4) Sell tickets. <br> 5) Profit.
+A. 1) Go to Japan. <br> &nbsp;&nbsp;&nbsp; 2) Find Godzilla. <br> &nbsp;&nbsp;&nbsp; 3) Ask him to come fight your robot. <br> &nbsp;&nbsp;&nbsp; 4) Sell tickets. <br> &nbsp;&nbsp;&nbsp; 5) Profit.
 </br>
 
 <p>
@@ -54,11 +54,11 @@ A. It appears that your robot is in WALL-E mode. Preform a soft-reset and keep y
 
 <p>
 <b>Q: When I send my bot away to do tasks, it always replies with "I'll be back."</b><br>
-A. Your robot is the terminator. Dispose of your robot immediately.
+A. Your robot is the Terminator. Dispose of your robot immediately.
 
 <p>
 <b>Q: When I ask my robot for advice, it tells me to use the Schwartz. What's the Shwartz?</b><br>
-A. Please refer to the first question, but hide your copy of spaceballs, instead.
+A. Please refer to the first question, but hide your copy of Spaceballs, instead.
 
 <p>
 <b>Q: My robot sits and talks to my car. Why is my robot trying to talk to my car?</b><br>
