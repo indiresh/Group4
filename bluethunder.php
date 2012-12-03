ebbb294de4c19110a067c6366009f8a563836ce5
Edmund Russell <ejrussel@oakland.edu>
2012-12-03 12:08:45 -0500

    Added the info for my robot.

--- bluethunder.php
+++ bluethunder.php
@@ -2,19 +2,22 @@
 
 <h1>Blue Thunder</h1>
 <img src="http://www.secs.oakland.edu/~mjdawson/images/bluethunder.jpg" height="300" width="250" border="0" alt="Blue Thunder" align="left">
-Blue Thunder is comming soon!
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> 
+Blue Thunder is a state of the art robot.  He comes complete with artificial intelligence.  Smarter than any human, he can answer any question you give him. Utilizing a stored database of knowledge, he can also reach out to other sources for answers.  Once retrieved, he will store the information for future use.  This awesome robot has voice recognition software built in.  This allows you to speak to him and never have to type commands.  This robot is for superior
+to any robot on the market today when it comes to intelligence.
+<p>Blue thunder has been used by NASA to engineer the special spacecraft used to transport itself to Mars.  There he was able to collect data never able to be retrieved by humans.  This robot will pay for itself after purchased, 
+	Literally, he can predict the stock market with 98% accuracy.  Therefore if allowed, he will invest your money and earn it right back for you.</p>
 <p>
+
 <h2>Product Specifications</h2>
-<b>Dimensions:</b>     			<br>
-<b><span class="droptext">Robot Mass:<span style="width:500px;">How much the robot weighs.</span></span></b> <br>
-<b>Operating temperature range:</b>			<br>
-<b><span class="droptext">Frame:<span style="width:500px;">What the robot is made from.</span></span></b> <br>
-<b><span class="droptext">Class:<span style="width:500px;">The general type of robot.</span></span></b>  <br>
-<b><span class="droptext">Power:<span style="width:500px;">What this robot uses for power.</span></span></b> <br>
-<b><span class="droptext">Full Recharge Rate:<span style="width:500px;">How long it takes this robot to recharge. (If applicable)</span></span></b>  <br>
+<b>Dimensions:</b> 6 Feet Tall x 2 Feet Wide    			<br>
+<b><span class="droptext">Robot Mass:<span style="width:500px;">How much the robot weighs.</span></span></b>300 lbs <br>
+<b>Operating temperature range:</b>98.6 F			<br>
+<b><span class="droptext">Frame:<span style="width:500px;">What the robot is made from.</span></span></b>Titanium and Aluminium<br>
+<b><span class="droptext">Class:<span style="width:500px;">The general type of robot.</span></span></b>Intelligence  <br>
+<b><span class="droptext">Power:<span style="width:500px;">What this robot uses for power.</span></span></b>Lithium Batteries <br>
+<b><span class="droptext">Full Recharge Rate:<span style="width:500px;">How long it takes this robot to recharge. (If applicable)</span></span></b>1 Hour  <br>
 <b>Color(s):</b> Blue <br>
 
-
+<h2>Price: $20,000,000.00</h2>
 
 <?php include ('footer.php'); ?>
